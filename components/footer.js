79504606d41483e447a1c1ba513c89f535880f2f
Nina Rocket <61925595+NinaRocket@users.noreply.github.com>
2023-08-05 14:36:25 -0700

    initialize

---
+++ components/footer.js
@@ -0,0 +1,45 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <section class="footer">
+    <!-- Footer -->
+    <footer class="text-center text-white" style="background-color: #ff9507;">
+      <!-- Grid container -->
+      <div class="container p-4 pb-0">
+        <!-- Section: CTA -->
+        <section class="">
+          <p class="d-flex justify-content-center align-items-center">
+            <span class="me-3">Copyright © 2023 Jazmine's Journey</span>
+            
+          </p>
+        </section>
+        <!-- Section: CTA -->
+      </div>
+      <!-- Grid container -->
+  
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        <span class="me-3">Created by enRocket Dev <img src="../../Assets/Nina Rocket Icon Black.png" style="height:30px;"></span>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+  </section>
+  <style>
+  .footer {
+    position: sticky;
+    top: 100vh;
+    margin-top:20px;
+  }
+  </style>
+   `;
+  }
+}
+
+  customElements.define('footer-component', Footer);
+
+ 
---
+++ components/header.js
@@ -0,0 +1,46 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="../../index.html"><img src="../../Assets/cancer-ribbon-clip-art-24.png" height="45px;"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Links
+                </a>
+                <ul class="dropdown-menu">
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="../Pages/JazminesStory.html">Jazmine's Story</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="../Pages/hikingevents.html">Hiking Events</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="../Pages/support.html">Support</a></li>
+                 
+                </ul>
+              </li>
+            </ul>
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">About</a>
+          </li>
+        </ul>
+       
+      </div>
+    </div>
+  </nav>
+    `;
+  }
+}
+
+  customElements.define('header-component', Header);
+
+ 
