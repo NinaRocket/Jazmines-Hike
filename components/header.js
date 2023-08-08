1a48f1d58fa18b01a17d6241451e711737e925ea
Nina Rocket <61925595+NinaRocket@users.noreply.github.com>
2023-08-07 19:47:26 -0700

    deploy live

--- components/header.js
+++ components/header.js
@@ -30,7 +30,7 @@ class Header extends HTMLElement {
             </ul>
         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../Pages/JazminesStory.html">About</a>
+            <a class="nav-link active" aria-current="page" href="../Pages/support.html">Support</a>
           </li>
         </ul>
        
