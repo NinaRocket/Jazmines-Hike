ca7112836286232e7aae4c5dd9e9459271070e87
Nina Rocket <61925595+NinaRocket@users.noreply.github.com>
2024-05-26 12:24:05 -0700

    impact page, new navbar, updated fundraisers

--- components/header.js
+++ components/header.js
@@ -6,57 +6,79 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="../../index.html">Jazmine's <img src="../../Assets/Jazmines-Journey-Logo.PNG" height="45px;"> Journey</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              
-                </ul>
-            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="../../index.html">Jazmine's <img src="../../Assets/Jazmines-Journey-Logo.PNG"
+          height="45px;"> Journey</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-            <li class="nav-item"><a class="nav-link active" href="./JazminesStory.html">Jazmine's Story</a></li>
+        </ul>
 
-            <li class="nav-item"><a class="nav-link active" href="./upcomingEvents.html">Events</a></li>
 
-            
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./support.html">Support</a>
-              </li>
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+             
+
+              <li class="nav-item"><a class="nav-link active" href="./upcomingEvents.html">Events</a></li>
 
               <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Online Fundraisers
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="./JazminesPiggies.html">Piggy Fundraiser</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="./statefortyeight.html">State Forty Eight Collab</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="https://www.zeffy.com/en-US/donation-form/b6a57cd2-dc35-4595-80bf-47d4561e4d72" target="_blank">Link to General Donation</a></li>
-              </ul>
-            </li>
-
-              <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./joinUs.html">Join Us</a>
-            </li>
-
-              <li class="nav-item"><a class="nav-link active" href="./PastEvents.html">Past Events</a></li>
-
-              <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="./thankyou.html">Thank You</a>
-            </li>
-
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./Contact.html">Contact & Subscribe</a>
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Online Fundraisers
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="./JazminesPiggies.html">Piggy Fundraiser</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="./statefortyeight.html">State Forty Eight Collab</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="https://www.zeffy.com/en-US/donation-form/b6a57cd2-dc35-4595-80bf-47d4561e4d72" target="_blank">Link to General Donation</a></li>
+                </ul>
               </li>
-            </ul>
-           
-          </div> 
-        </div>
-      </nav>
+
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Impact
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="./giving.html">Your Impact</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="./PastEvents.html">Past Events</a></li>     
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" aria-current="page" href="./thankyou.html">Thank You</a></li> 
+                </ul>
+              </li>
+
+      
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  About Jazmine's Journey
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="./JazminesStory.html">Jazmine's
+                    Story</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li>
+                    <a class="dropdown-item" aria-current="page" href="./joinUs.html">Join Us</a>
+                  </li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li>
+                    <a class="dropdown-item" aria-current="page" href="./Contact.html">Contact & Subscribe</a>
+                  </li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li>
+                    <a class="dropdown-item" aria-current="page" href="./support.html">Support</a>
+                  </li>
+                </ul>
+              </li>
+        
+        </ul>
+
+      </div>
+    </div>
+  </nav>
     `;
   }
 }
