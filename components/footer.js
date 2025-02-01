89c3baf49d9757105076f50eab90d27f9bfa998e
Nina Rocket <61925595+NinaRocket@users.noreply.github.com>
2025-02-01 15:13:29 -0700

    added social media icons

--- components/footer.js
+++ components/footer.js
@@ -4,6 +4,15 @@ class Footer extends HTMLElement {
     }
 
   connectedCallback() {
+
+    if (!document.getElementById('fontAwesomeStylesheet')) {
+      const fontAwesomeCSS = document.createElement('link');
+      fontAwesomeCSS.id = 'fontAwesomeStylesheet';  // Prevent duplicates
+      fontAwesomeCSS.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css';
+      fontAwesomeCSS.rel = 'stylesheet';
+      document.head.appendChild(fontAwesomeCSS);
+    }
+
     this.innerHTML = `
     <section class="footer">
     <!-- Footer -->
@@ -12,6 +21,16 @@ class Footer extends HTMLElement {
       <div class="container p-4 pb-0">
         <!-- Section: CTA -->
         <section class="">
+         Follow Us on Social Media
+          <div style="padding: 5px;">
+          <a href="https://www.youtube.com/@_jazminesjourney_" target="_blank"><i class="fa-brands fa-youtube" style="color:red;font-size:25px;padding: 5px;"></i></a>
+          <a href="https://www.instagram.com/_jazminesjourney_/#" target="_blank"><i class="fa-brands fa-instagram" style="color:purple;font-size:25px;padding: 5px;"></i></a>
+          <a href="https://jazminesjourneyblog.wordpress.com" target="_blank"><i class="fa-brands fa-wordpress" style="color:darkblue;font-size:25px;padding: 5px;"></i></a>
+          <a href="https://www.tiktok.com/@jazminesjourney" target="_blank"><i class="fa-brands fa-tiktok" style="color:white;font-size:25px;padding: 5px;"></i></a>
+          <a href="https://www.facebook.com/JazminesJourney" target="_blank"><i class="fa-brands fa-facebook" style="color:blue;font-size:25px;padding: 5px;"></i></a>
+     
+        </div>
+        
           <p class="d-flex justify-content-center align-items-center">
             <span class="me-3">Copyright © 2025 Jazmine's Journey</span>
             
@@ -34,6 +53,7 @@ class Footer extends HTMLElement {
       <!-- Grid container -->
   
       <!-- Copyright -->
+    
       <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
         <span class="me-3">Created by enRocket Dev <img src="../../Assets/Nina Rocket Icon Black.png" style="height:30px;"></span>
       </div>
@@ -49,9 +69,8 @@ class Footer extends HTMLElement {
   }
   </style>
    `;
-  }
+  } 
 }
-
-  customElements.define('footer-component', Footer);
+customElements.define('footer-component', Footer);
 
  
